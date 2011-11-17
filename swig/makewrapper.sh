77b42076278f4d94ae3388ad8c9a7b4bc22d41a1
Paweł Kędzia <Pawel.Kedzia@pwr.wroc.pl>
2011-11-17 11:00:17 +0100

    Updated maker for swig wrappers

--- swig/makewrapper.sh
+++ swig/makewrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 if [ ${#} -eq 0 ]
 then
@@ -9,8 +9,8 @@ fi
 UPPER=`echo ${1} | tr '[:lower:]' '[:upper:]'`
 LOWER=`echo ${1} | tr '[:upper:]' '[:lower:]'`
 CLNAM=${1}
-OUTFILE=libcorpus${LOWER}.i
+OUTFILE=${LOWER}.i
 
 cat swig_template_corpus2.template  | sed s/TEMPLATE/${UPPER}/g | sed s/Template/${CLNAM}/g | sed s/template/${LOWER}/g > ${OUTFILE}
 
-echo -e "\e[1;32m${OUTFILE} was generated. Don't forget to add ${OUTFILE} to Makefile (or CMake)!\e[0m"
+echo -e "\e[1;32m${OUTFILE} has been generated. Don't forget to add ${OUTFILE} to Makefile (or CMake)!\e[0m"
