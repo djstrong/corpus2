e2aa981d010452540bacfdccf9aa67eb7e75599f
Paweł Kędzia <Pawel.Kedzia@pwr.wroc.pl>
2011-12-29 12:21:49 +0100

    Added DocumentCorpusReader

--- libcorpus2_whole/io/documentcorpusreader.h
+++ libcorpus2_whole/io/documentcorpusreader.h
@@ -1,56 +1,46 @@
-#ifndef LIBCORPUS2_WHOLE_CORPUSREADER_H
-#define LIBCORPUS2_WHOLE_CORPUSREADER_H
+#ifndef LIBCORPUS2_WHOLE_DOCUMENTCORPUSREADER_H
+#define LIBCORPUS2_WHOLE_DOCUMENTCORPUSREADER_H
 
 #include <string>
 #include <boost/shared_ptr.hpp>
-
-#include <libcorpus2_whole/io/docreaderi.h>
-#include <libcorpus2_whole/io/docreader.h>
 #include <libcorpus2_whole/corpus.h>
+#include <libcorpus2_whole/io/reader_i.h>
 
 namespace Corpus2 {
 namespace whole {
 
 /**
- * CorpusReader is a corpus-like reader
+ * Reads "document-like cropus"
+ * Reads all documents in given file and returns their Corpus representation
  */
-class CorpusReader
+class DocumentCorpusReader : public CorpusReaderI
 {
 public:
-	/**
-	 * @arg corpus_type may be:
-	 *  - document (contains relations)
-	 *  - poliqarp
-	 */
-	CorpusReader(const Tagset& tagset, const std::string& corpus_type);
+	DocumentCorpusReader(const Tagset& tagset);
 
 	/**
 	 * Reads corpus from given path
 	 * @arg corpus_file Path to file contains paths to corpus files.
-	 *      Depend on corpus type, each line in this file should contains only
-	 *      path to one document from corpus or path to (in particular DocReader)
-	 *      relations and annotatons (in one line, first is path to annotations
-	 *      and second are relations -- these paths, should be separated by semicolon)
+	 *      Each lines in given corpus file contains two paths, to relations
+	 *      and annotatons (in one line, first is path to annotations
+	 *      and second are relations -- these paths, should be separated
+	 *      by semicolon)
 	 * @return Readed corpus
 	 */
 	boost::shared_ptr<Corpus> read(const std::string& corpus_file);
 
-protected:
-	/// Tagset to use, sets only onece in constructor
-	const Tagset& tagset_;
-
-	/// Type of corpus, sets only once in constructor
-	const std::string& corpus_type_;
+	/**
+	 * Sets options for readers (relation reader and/or ccl reader).
+	 * Available options:
+	 *  - autogen_sent_id -- for automatically generation identifiers of sentences
+	 */
+	void set_option(const std::string& option);
 
 private:
-	/// Returns reader based on corpus type (poliqarp/document)
-	boost::shared_ptr<DocumentReaderI> get_reader_by_type(
-		const std::string &type,
-		const std::string &ann_path,
-		const std::string &rel_path = "");
+	const Tagset& tagset_;
 };
 
 } // whole ns
 } // Corpus2 ns
 
-#endif // LIBCORPUS2_WHOLE_CORPUSREADER_H
+#endif // LIBCORPUS2_WHOLE_DOCUMENTCORPUSREADER_H
