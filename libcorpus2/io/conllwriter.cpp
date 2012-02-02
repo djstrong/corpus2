21618b8d9ed299bc09383c0d9defc7693a82e3b3
dominik <dominik@trzeciodlewej.(none)>
2012-02-02 16:39:11 +0100

    conllwriter fix

--- libcorpus2/io/conllwriter.cpp
+++ libcorpus2/io/conllwriter.cpp
@@ -1,6 +1,7 @@
 #include "conllwriter.h"
 #include <libpwrutils/foreach.h>
 #include <boost/algorithm/string.hpp>
+#include <algorithm>
 
 
 namespace Corpus2 {
@@ -26,12 +27,13 @@ void ConllWriter::write_token(const Token &t)
 	os()<<lex.lemma_utf8()+"\t";
 	std::string tag = myTagset.tag_to_string(lex.tag());
 	std::vector<std::string> strs;
+	std::transform(tag.begin(), tag.end(), tag.begin(), ::tolower);
 	boost::split(strs, tag, boost::is_any_of(":"));
-	os()<<strs[0]<<"\t"<<strs[0]<<"\t";
-	if(strs.size()>1)
+	os()<<strs[1]<<"\t"<<strs[0]<<"\t";
+	if(strs.size()>2)
 	{
 		size_t i;
-		for(i=1;i<strs.size()-1;i++)
+		for(i=2;i<strs.size()-1;i++)
 		{
 			os()<<strs[i]<<"|";
 		}
@@ -67,19 +69,6 @@ void ConllWriter::do_header()
 
 void ConllWriter::do_footer()
 {
-}
-
-std::string ConllWriter::convert_tag(std::string tag)
-{
-	if(tag.compare("adja")==0||tag.compare("adjc")==0||tag.compare("adjp")==0||tag.compare("padj")==0||tag.compare("pact")==0||tag.compare("ppas")==0)
-		return "adj";
-	if(tag.compare("padv")==0||tag.compare("pant")==0||tag.compare("pcon")==0)
-		return "adv";
-	if(tag.compare("bedzie")==0||tag.compare("fin")==0||tag.compare("imps")==0||tag.compare("impt")==0||tag.compare("inf")==0||tag.compare("praet")==0||tag.compare("pred")==0||tag.compare("winien")==0)
-		return "verb";
-	if(tag.compare("psubst")==0||tag.compare("depr")==0||tag.compare("ger")==0||tag.compare("ppron3")==0||tag.compare("ppron12")==0||tag.compare("siebie")==0)
-		return "subst";
-	return tag;
 }
 
 } /* end ns Corpus2 */
