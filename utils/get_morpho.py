3559fa1b5aff409a0b9587a788872986b76e7891
Adam Radziszewski <adam.radziszewski@pwr.wroc.pl>
2013-09-30 16:18:49 +0200

    get_morpho script now more flexible and useful to work with CST lemmatiser

--- utils/get_morpho.py
+++ utils/get_morpho.py
@@ -30,19 +30,41 @@ import corpus2
 class Analyser:
 	"""Morphological dictionary resulting from reading a corpus.
 	Provides consume function that updates the data with the given token."""
-	def __init__(self, tagset, case_sensitive):
+	def __init__(self, tagset, keep_case_of, gather_freq = False):
+		assert keep_case_of in ['orth', 'lemma', 'both', 'none']
+		keep_lemma_case = (keep_case_of in ['lemma', 'both'])
+		keep_orth_case = (keep_case_of in ['orth', 'both'])
+		self.get_form = (
+			(lambda form: form)
+			if keep_orth_case
+			else (lambda form: form.lower())
+		)
+		self.get_lemma = (
+			(lambda lem: lem)
+			if keep_lemma_case
+			else (lambda form: form.lower())
+		)
 		self.tagset = tagset
 		self.unk_tag = self.tagset.make_ign_tag()
-		self.get_form = (lambda form: form) if case_sensitive else (lambda form: form.lower())
+		self.gather_freq = gather_freq
 		self.data = {}
+		self.freq = {} # form-tag frequency dict
 	
 	def _add_one(self, ready_form, lex):
 		"""Adds tag and lemma from the given lexeme."""
 		if ready_form not in self.data:
 			self.data[ready_form] = set()
 		self.data[ready_form].add(
-			(unicode(lex.lemma()), self.tagset.tag_to_string(lex.tag()))
+			(
+				self.get_lemma(unicode(lex.lemma())),
+				self.tagset.tag_to_string(lex.tag()))
 		)
+		
+		if self.gather_freq:
+			freq_key = (
+				ready_form,
+				self.tagset.tag_to_string(lex.tag()))
+			self.freq[freq_key] = self.freq.get(freq_key, 0) + 1
 	
 	def consume(self, tok):
 		ready_form = self.get_form(unicode(tok.orth()))
@@ -60,10 +82,19 @@ class Analyser:
 		output = codecs.open(fname, 'wb', 'utf-8')
 		self.write(output)
 		output.close()
+	
+	def save_freq(self, freq_fname):
+		freq_output = codecs.open(freq_fname, 'wb', 'utf-8')
+		for what, count in sorted(
+				self.freq.items(),
+				key=lambda item: (-item[1], item[0])
+				):
+			freq_output.write(u'%d\t%s\t%s\n' % (count, what[0], what[1]))
+		freq_output.close()
 
 def get_morpho(options, corpname, outfname):
 	tagset = corpus2.get_named_tagset(options.tagset)
-	anal = Analyser(tagset, options.case_sens)
+	anal = Analyser(tagset, options.keep_case, bool(options.freq_list))
 	rdr = corpus2.TokenReader.create_path_reader(options.input_format, tagset, corpname)
 	while True:
 		tok = rdr.get_next_token()
@@ -72,6 +103,8 @@ def get_morpho(options, corpname, outfname):
 		anal.consume(tok)
 	del rdr
 	anal.save(outfname)
+	if options.freq_list:
+		anal.save_freq(options.freq_list)
 
 def go():
 	parser = OptionParser(usage=descr)
@@ -84,7 +117,14 @@ def go():
 	parser.add_option('-s', '--sep', type='string', action='store',
 		dest='sep', default='\t',
 		help='set the separator used in morpho file; default: tab character')
-	parser.add_option('-c', '--case-sensitive', action='store_true', default=False, dest='case_sens')
+	parser.add_option(
+		'-c', '--respect-case-for', type='string', action='store',
+		dest='keep_case', default='lemma',
+		help='keep letter case for: none, orth, lemma or both; default: lemma')
+	parser.add_option(
+		'--freq', type='string', action='store',
+		dest='freq_list', default='',
+		help='also extract orth-tag frequency list (useful for CST lemmatiser) and save to given file')
 	parser.add_option('-q', '--quiet', action='store_false', default=True, dest='verbose')
 	(options, args) = parser.parse_args()
 	
