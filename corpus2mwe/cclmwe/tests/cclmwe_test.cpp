5e8776a1c780cca8549d2d1043425b294564ba49
A. Janz <arkadiusz.janz@pwr.edu.pl>
2017-03-01 16:10:02 +0100

    recovered tests for cclmwereader- adapted tests to new implementation of MWEReader

--- corpus2mwe/cclmwe/tests/cclmwe_test.cpp
+++ corpus2mwe/cclmwe/tests/cclmwe_test.cpp
@@ -36,16 +36,18 @@ struct io_fixture {
 };
 
 void write_ccl(std::string, TokenReaderPtr, const Tagset &);
-bool mwe_exists(Sentence::Ptr, std::string, std::string, const Tagset &);
+bool merged_mwe_exists(Sentence::Ptr, std::string, std::string, const Tagset &);
+bool annotated_mwe_exists(Sentence::Ptr, std::string, const Tagset &);
 
 
 BOOST_AUTO_TEST_SUITE( io_test )
 
-    BOOST_FIXTURE_TEST_CASE( mwes, io_fixture ) {
+    BOOST_FIXTURE_TEST_CASE( mwes_merged, io_fixture ) {
         /* 1) create a CclReader and read first sentence of input file
          * 2) create reader with MWE support and get also the first sentence
+         *      - reader uses token merging instead of annotations
          * 3) compare sizes of sentences (MWE sentence should be shorter
-         *      because of MWE tokens)
+         *      because of merged MWE tokens)
          */
         const Tagset & tagset = get_named_tagset(tagset_name);
 
@@ -53,17 +55,19 @@ BOOST_AUTO_TEST_SUITE( io_test )
         Sentence::Ptr s1 = ccl_reader->get_next_sentence();
 
         boost::shared_ptr<CclMWEReader> cclmwer(
-                new CclMWEReader(tagset, infile)
+                new CclMWEReader(infile, tagset)
         );
+        cclmwer->use_annotations(false);
         Sentence::Ptr s2 = cclmwer->reader()->get_next_sentence();
         BOOST_REQUIRE(s1->size() > s2->size());
     }
 
-    BOOST_FIXTURE_TEST_CASE( without_mwes, io_fixture ) {
+    BOOST_FIXTURE_TEST_CASE( mwes_annotated, io_fixture ) {
         /* 1) create a CclReader and read first sentence of input file
-         * 2) create reader without MWE support and get the first sentence
-         * 3) compare sizes of sentences (sentences should have same
-	 * 	length because of the same reader type)
+         * 2) create reader with MWE support and get also the first sentence
+         *      - reader uses annotations as default instead of merging
+         * 3) compare sizes of sentences (MWE sentence should be the same
+         *      length, because tokens are not merged - we annotated them)
          */
         const Tagset & tagset = get_named_tagset(tagset_name);
 
@@ -71,13 +75,15 @@ BOOST_AUTO_TEST_SUITE( io_test )
         Sentence::Ptr s1 = ccl_reader->get_next_sentence();
 
         boost::shared_ptr<CclMWEReader> cclmwer(
-                new CclMWEReader(tagset, infile, false)
+                new CclMWEReader(infile, tagset)
         );
+        cclmwer->use_annotations(true);
         Sentence::Ptr s2 = cclmwer->reader()->get_next_sentence();
         BOOST_REQUIRE(s1->size() == s2->size());
     }
 
-    BOOST_FIXTURE_TEST_CASE( ccl_mwe, io_fixture ) {
+
+    BOOST_FIXTURE_TEST_CASE( ccl_mwe_merged, io_fixture ) {
         /* 1) create a reader with MWE support
          * 2) reader loads MWEs dictionary and reads CCL document with MWEs
          * 3) write CCL with extracted MWEs
@@ -87,21 +93,44 @@ BOOST_AUTO_TEST_SUITE( io_test )
         boost::shared_ptr<CclReader> ccl_reader(new CclReader(tagset, infile));
 
         boost::shared_ptr<CclMWEReader> cclmwer(
-                new CclMWEReader(tagset, infile)
+                new CclMWEReader(infile, tagset)
         );
+        cclmwer->use_annotations(false);
         write_ccl(outfile, cclmwer->reader(), tagset);
         ccl_reader.reset(new CclReader(tagset, outfile));
         Sentence::Ptr sent = ccl_reader->get_next_sentence();
         BOOST_FOREACH(auto& mwe, mwes) {
-        	BOOST_REQUIRE(mwe_exists(sent, mwe.first, mwe.second, tagset));
+        	BOOST_REQUIRE(merged_mwe_exists(sent, mwe.first, mwe.second, tagset));
         }
     }
+
+    BOOST_FIXTURE_TEST_CASE( ccl_mwe_annotated, io_fixture ) {
+        /* 1) create a reader with MWE support
+         * 2) reader loads MWEs dictionary and reads CCL document with MWEs
+         * 3) write CCL with extracted MWEs
+         * 4) read output CCL and check if MWEs exist
+         */
+        const Tagset & tagset = get_named_tagset(tagset_name);
+        boost::shared_ptr<CclReader> ccl_reader(new CclReader(tagset, infile));
+
+        boost::shared_ptr<CclMWEReader> cclmwer(
+                new CclMWEReader(infile, tagset)
+        );
+        cclmwer->use_annotations(true);
+        write_ccl(outfile, cclmwer->reader(), tagset);
+        ccl_reader.reset(new CclReader(tagset, outfile));
+        Sentence::Ptr sent = ccl_reader->get_next_sentence();
+        BOOST_FOREACH(auto& mwe, mwes) {
+        	BOOST_REQUIRE(annotated_mwe_exists(sent, mwe.second, tagset));
+        }
+    }
+
 
     BOOST_FIXTURE_TEST_CASE( mwedict_not_exists, io_fixture ) {
         const Tagset & tagset = get_named_tagset(tagset_name);
 	std::string mwe_dict = "./some_not_existing_mwe_dict";
         BOOST_CHECK_THROW(
-                new CclMWEReader(tagset, infile, mwe_dict),
+                new CclMWEReader(infile, tagset, mwe_dict),
                 Corpus2::Corpus2Error
         );
     }
@@ -119,7 +148,7 @@ void write_ccl(std::string outfile, TokenReaderPtr reader, const Tagset & tagset
     }
 }
 
-bool mwe_exists(Sentence::Ptr s, std::string mwe_tok, std::string mwe_base,
+bool merged_mwe_exists(Sentence::Ptr s, std::string mwe_tok, std::string mwe_base,
                 const Tagset & tagset)
 {
     std::string t_tok;
@@ -130,6 +159,25 @@ bool mwe_exists(Sentence::Ptr s, std::string mwe_tok, std::string mwe_base,
         if (t_tok == mwe_tok && t_base == mwe_base) {
             return true;
         }
+    }
+    return false;
+}
+
+bool annotated_mwe_exists(Sentence::Ptr s, std::string mwe_base,
+        const Tagset & tagset) 
+{
+    typedef boost::shared_ptr<Corpus2::AnnotatedSentence> AnnotatedSentencePtr;
+    typedef boost::shared_ptr<Corpus2::TokenMetaData> TokenMetaDataPtr;
+    AnnotatedSentencePtr ann_s = Corpus2::AnnotatedSentence::wrap_sentence(s);
+    BOOST_FOREACH(Token *t, ann_s->tokens()) {
+        if (t->get_metadata()) {
+            TokenMetaDataPtr md = t->get_metadata();
+            if (md->has_attribute("mwe_base")) {
+                std::string base = md->get_attribute("mwe_base");
+                if (mwe_base == base)
+                    return true;
+            }
+        } 
     }
     return false;
 }
